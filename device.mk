3eea989bbcba343913f4537d5f427bd630ec5578
Pig <pig.priv@gmail.com>
2020-12-17 02:10:53 +0800

    beryllium: Shorten wait time for services exit to optimize shutdown time
    Change-Id: I66581d0d50a675e28d15a6fa58e728cb5834ab09

--- device.mk
+++ device.mk
@@ -495,6 +495,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Shutdown
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.build.shutdown_timeout=2
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
