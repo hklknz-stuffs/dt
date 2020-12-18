23de5967123777444d5fb8ad0f9b751feec69b6f
Alastair Donaldson <afdx@google.com>
2020-12-18 13:53:59 +0000

    beryllium: Add OpenGL ES and update Vulkan dEQP feature flags
    Populates a new	feature	flag to	record the level of OpenGL ES dEQP
    test support.  Updates the analogous Vulkan dEQP feature flag to the
    value for the next dessert release.
    
    Bug: 159841535
    Bug: 175706433
    Merged-In: Ib0fb8f83732d0b3f56bb11a97c84fdc7727c1026
    Change-Id: Ic97a65fba95a67e3e16cedd0eb4f3f209f645421

--- device.mk
+++ device.mk
@@ -443,8 +443,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
-    frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
-    frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
+    frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml
 
 # Platform
