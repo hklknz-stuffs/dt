2f43e82d26703e523a7e5ac8655ddd48a3fa591f
Vaisakh Murali <mvaisakh@statixos.com>
2023-12-26 21:22:20 +0530

    beryllium: Enable Multi-Gen LRU
    Signed-off-by: Shikhar <krtonia@gmail.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- device.mk
+++ device.mk
@@ -553,3 +553,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Multi-Gen
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.device_config.mglru_native.lru_gen_config=core
