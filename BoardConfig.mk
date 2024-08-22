22ae89570196ce0b7b31da6341eeaecfef36acf4
haikaru17 <haikalluthfianinobalukia@proton.me>
2024-08-22 09:06:35 +0000

    beryllium: AfterLife Savuvukna Welll!

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/afterlife/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 # Kernel
--- afterlife_beryllium.mk
+++ afterlife_beryllium.mk
@@ -10,18 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
+-include vendor/extra/product.mk
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_beryllium
+PRODUCT_NAME := afterlife_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCOPHONE F1
 PRODUCT_MANUFACTURER := Xiaomi
-
+TARGET_FACE_UNLOCK_SUPPORTED := true
+AFTERLIFE_MAINTAINER := lhklknz
+USE_PIXEL_CHARGING := true
 PRODUCT_SYSTEM_NAME := beryllium
 
 BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
