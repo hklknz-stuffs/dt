098d0d1aaa5a30beae35b01d0a695794c9c824f4
SonicBSV <sonicbsv@ya.ru>
2024-08-28 19:51:31 +0300

    beryllium: Grant android.permission.WRITE_DEVICE_CONFIG to gms Grant android.permission.WRITE_DEVICE_CONFIG to com.google.android.gms package.
    TEST: run gts -m GtsDeviceConfigTestCases -t
    com.google.android.deviceconfig.gts.DeviceConfigApiPermissionTests#testWriteDeviceConfigPermission
    
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/13666
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: Tian Yang <yang.tian@nxp.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -253,6 +253,10 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# GMS Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp.permissions-gms.xml
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/gps/,$(TARGET_COPY_OUT_VENDOR)/etc)
