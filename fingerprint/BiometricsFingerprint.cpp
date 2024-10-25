LineageOS/android_device_xiaomi_sdm845-common

sdm845-common: fingerprint: Set NIT value on sysfs notification
Change-Id: Ie4001b11f9a26e33a3ea0f1a8d48c1346be16b76

fingerprint/BiometricsFingerprint.cpp
@@ -27,12 +27,41 @@
 #include <inttypes.h>
 #include <unistd.h>
 
+#include <fcntl.h>
+#include <poll.h>
+
 #include <cmath>
 #include <fstream>
+#include <thread>
 
 #define COMMAND_NIT 10
 #define PARAM_NIT_630_FOD 1
 #define PARAM_NIT_NONE 0
+
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display@20/fod_ui"
+
+namespace {
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        ALOGE("failed to seek fd, err:  %d", rc);
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        ALOGE("failed to read bool from fd, err:  %d", rc);
+        return false;
+    }
+
+    return c != '0';
+}
+
+}  // anonymous namespace
 
 namespace android {
 namespace hardware {
@@ -54,6 +83,33 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
     mDevice = openHal();
     if (!mDevice) {
         ALOGE("Can't open HAL module");
+    }
+
+    if (mIsUdfps) {
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                ALOGE("failed to open fd, err:  %d", fd);
+                return;
+           }
+
+           struct pollfd fodUiPoll = {
+                .fd = fd,
+                .events = POLLERR | POLLPRI,
+                .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    ALOGE("failed to poll fd, err:  %d", rc);
+                    continue;
+                }
+
+                mDevice->extCmd(mDevice, COMMAND_NIT,
+                         readBool(fd) ? PARAM_NIT_630_FOD : PARAM_NIT_NONE);
+            }
+        }).detach();
     }
 }
 
@@ -409,16 +465,10 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
-    if (mIsUdfps) {
-        mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_630_FOD);
-    }
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    if (mIsUdfps) {
-        mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_NONE);
-    }
     return Void();
 }
 

hklknz-stuffs/dt

fingerprint/BiometricsFingerprint.cpp
@@ -27,12 +27,41 @@
 #include <inttypes.h>
 #include <unistd.h>
 
+#include <fcntl.h>
+#include <poll.h>
+
 #include <cmath>
 #include <fstream>
+#include <thread>
 
 #define COMMAND_NIT 10
 #define PARAM_NIT_630_FOD 1
 #define PARAM_NIT_NONE 0
+
+#define FOD_UI_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui"
+
+namespace {
+
+static bool readBool(int fd) {
+    char c;
+    int rc;
+
+    rc = lseek(fd, 0, SEEK_SET);
+    if (rc) {
+        ALOGE("Failed to seek fd, err: %d", rc);
+        return false;
+    }
+
+    rc = read(fd, &c, sizeof(char));
+    if (rc != 1) {
+        ALOGE("Failed to read bool from fd, err: %d", rc);
+        return false;
+    }
+
+    return c != '0';
+}
+
+}  // anonymous namespace
 
 namespace android {
 namespace hardware {
@@ -54,6 +83,31 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
     mDevice = openHal();
     if (!mDevice) {
         ALOGE("Can't open HAL module");
+    } else if (sIsUdfps) {
+        std::thread([this]() {
+            int fd = open(FOD_UI_PATH, O_RDONLY);
+            if (fd < 0) {
+                ALOGE("Failed to open fd, err: %d", fd);
+                return;
+           }
+
+           struct pollfd fodUiPoll = {
+                .fd = fd,
+                .events = POLLERR | POLLPRI,
+                .revents = 0,
+            };
+
+            while (true) {
+                int rc = poll(&fodUiPoll, 1, -1);
+                if (rc < 0) {
+                    ALOGE("Failed to poll fd, err: %d", rc);
+                    continue;
+                }
+
+                mDevice->extCmd(mDevice, COMMAND_NIT,
+                         readBool(fd) ? PARAM_NIT_630_FOD : PARAM_NIT_NONE);
+            }
+        }).detach();
     }
 }
 
@@ -409,16 +463,10 @@ Return<bool> BiometricsFingerprint::isUdfps(uint32_t /* sensorId */) {
 
 Return<void> BiometricsFingerprint::onFingerDown(uint32_t /* x */, uint32_t /* y */,
                                                 float /* minor */, float /* major */) {
-    if (sIsUdfps) {
-        mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_630_FOD);
-    }
     return Void();
 }
 
 Return<void> BiometricsFingerprint::onFingerUp() {
-    if (sIsUdfps) {
-        mDevice->extCmd(mDevice, COMMAND_NIT, PARAM_NIT_NONE);
-    }
     return Void();
 }
 